ea462b880793de5abd897c7e728ee8a9de274dd9
baolongee <2859375495@qq.com>
2024-06-27 11:17:49 +0800

    Site updated: 2024-06-27 11:17:44

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/06/26/first/","2024/05/06/hello-world/","2024/06/26/two/"];function toRandomPost(){
+var posts=["2024/06/26/first/","2024/05/06/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };

c44d10988f89608fe871bc91ea2a57191444867a
baolongee <2859375495@qq.com>
2024-07-11 11:39:38 +0800

    Site updated: 2024-07-11 11:39:33

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/06/26/first/"];function toRandomPost(){
+var posts=["2024/06/26/first/","2024/07/11/日记/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };

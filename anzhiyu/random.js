62b63b4aaf735e7d196305e5252dd25bdd718f01
baolongee <2859375495@qq.com>
2024-09-02 09:32:34 +0800

    Site updated: 2024-09-02 09:32:28

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/06/26/first/","2024/07/11/日记/"];function toRandomPost(){
+var posts=["2024/06/26/first/","2024/07/11/日记/","2024/09/02/日记2/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
